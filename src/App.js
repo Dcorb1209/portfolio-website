47188a192b1e2d7ddca93507a3d9f7438d658d0c
Steve Brundage <brundage12@gmail.com>
2019-05-08 16:54:40 -0400

    Added functionality to navbar. Project Cards started

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Landing from './Pages/Landing';
+import Projects from './Pages/Projects';
+import About from './Pages/About';
 import EditProject from './Pages/EditProject';
 import './App.css';
 
@@ -9,6 +11,8 @@ function App() {
     <Router>
       <div>
         <Route path="/" exact component={Landing} />
+        <Route path='/about' component={About} />
+        <Route path="/projects" component={Projects} />
         <Route path="/EditProject/" component={EditProject} />
       </div>
     </Router>
